3bd7b16f38ff9b1a410ed8cffcd4031c1dc30840
Nile Jocson <atraphaxiae@gmail.com>
2026-07-30 07:06:37 +0800

    Add xdg portal

--- configuration.nix
+++ configuration.nix
@@ -45,6 +45,12 @@
 
 	security.polkit.enable = true;
 
+	xdg.portal = {
+		enable = true;
+		extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+		config.common.default = "gtk";
+	};
+
 	services = {
 		displayManager.ly.enable = true;
 
