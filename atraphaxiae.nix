a101948830d3d4e07ee26449e3d927bdb8085af9
Nile Jocson <atraphaxiae@gmail.com>
2026-07-30 06:08:11 +0800

    Add firefox

--- atraphaxiae.nix
+++ atraphaxiae.nix
@@ -78,6 +78,19 @@
     };
   };
 
+  programs.firefox = {
+    enable = true;
+    profiles.atraphaxiae = {
+      isDefault = true;
+      settings = {
+        "browser.shell.checkDefaultBrowser" = false;
+        "signon.rememberSignons" = false;
+        "extensions.InstallTrigger.enabled" = false;
+        "extensions.autoDisableScopes" = 0;
+      };
+    };
+  };
+
   programs.git = {
     enable = true;
     signing = {
