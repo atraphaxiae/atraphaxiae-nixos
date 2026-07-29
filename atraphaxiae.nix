e0514ffde9fc6416f47ab15ebfe669ab48be2cb3
Nile Jocson <atraphaxiae@gmail.com>
2026-07-30 06:15:08 +0800

    Add vscode

--- atraphaxiae.nix
+++ atraphaxiae.nix
@@ -104,6 +104,43 @@
     };
   };
 
+  programs.vscode = {
+    enable = true;
+    mutableExtensionDir = false;
+    profiles.default = {
+      extensions = with pkgs.vscode-extensions; [
+        johnpapa.winteriscoming
+        eamodio.gitlens
+        ms-vscode.cpptools
+        llvm-vs-code-extensions.vscode-clangd
+        ms-vscode.cmake-tools
+        twxs.cmake
+        myriad-dreamin.tinymist
+        rust-lang.rust-analyzer
+        tamasfe.even-better-toml
+        jnoortheen.nix-ide
+      ];
+      userSettings = {
+        "chat.disableAIFeatures" = true;
+        "editor.fontLigatures" = true;
+        "editor.fontFamily" = "Iosevka Extended";
+        "editor.inlayHints.enabled" = "offUnlessPressed";
+        "editor.insertSpaces" = false;
+        "editor.renderWhitespace" = "all";
+        "editor.rulers" = [ 100 ];
+        "workbench.colorTheme" = "Winter is Coming (Dark Blue)";
+        "workbench.treeIndent" = 20;
+        "files.insertFinalNewline" = true;
+        "files.trimFinalNewlines" = true;
+        "files.trimTrailingWhitespace" = true;
+        "C_Cpp.intelliSenseEngine" = "disabled";
+        "[typst]"."editor.tabSize" = 4;
+        "[nix]"."editor.tabSize" = 4;
+        "[nix]"."editor.insertSpaces" = false;
+      };
+    };
+  };
+
   programs.rofi = {
     inherit terminal;
     enable = true;
