4dc4357a35f953fdb3ce8fff3edd2f4193812a3b
Nile Jocson <atraphaxiae@gmail.com>
2026-07-29 20:58:27 +0000

    Add gcc, clang, cmake, brightnessctl and tree to global packages, remove user packages in configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -29,16 +29,17 @@
 
   users.users.atraphaxiae = {
     isNormalUser = true;
-    extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
-    packages = with pkgs; [
-      tree
-    ];
+    extraGroups = [ "wheel" ];
   };
 
-  # environment.systemPackages = with pkgs; [
-  #   vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-  #   wget
-  # ];
+  environment.systemPackages = with pkgs; [
+    gcc
+    clang
+    clang-tools
+    cmake
+    brightnessctl
+    tree
+  ];
 
   programs.gnupg.agent = {
     enable = true;
