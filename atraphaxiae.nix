801579f1ad715b349590e52b582730251b76bb2e
Nile Jocson <atraphaxiae@gmail.com>
2026-07-30 08:24:24 +0800

    Fully set firefox as default browser

--- atraphaxiae.nix
+++ atraphaxiae.nix
@@ -68,12 +68,22 @@
 
 	xdg = {
 		mime.enable = true;
-		mimeApps.defaultApplications = {
-			"text/html" = "firefox.desktop";
-			"x-scheme-handler/http" = "firefox.desktop";
-			"x-scheme-handler/https" = "firefox.desktop";
-			"x-scheme-handler/about" = "firefox.desktop";
-			"x-scheme-handler/unknown" = "firefox.desktop";
+		mimeApps = {
+			enable = true;
+			defaultApplications = {
+				"text/html" = "firefox.desktop";
+				"x-scheme-handler/about" = "firefox.desktop";
+				"x-scheme-handler/chrome" = "firefox.desktop";
+				"x-scheme-handler/http" = "firefox.desktop";
+				"x-scheme-handler/https" = "firefox.desktop";
+				"x-scheme-handler/unknown" = "firefox.desktop";
+				"application/x-extension-htm" = "firefox.desktop";
+				"application/x-extension-html" = "firefox.desktop";
+				"application/x-extension-shtml" = "firefox.desktop";
+				"application/x-extension-xhtml" = "firefox.desktop";
+				"application/x-extension-xht" = "firefox.desktop";
+				"application/xhtml+xml" = "firefox.desktop";
+			};
 		};
 		userDirs = {
 			enable = true;
