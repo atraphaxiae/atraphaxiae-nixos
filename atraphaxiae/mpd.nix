bfe3b82ca36551cc9f63c90cc4242440a6c50e87
Nile Jocson <atraphaxiae@gmail.com>
2026-08-22 11:08:34 +0800

    Add mpd-discord-rpc

--- atraphaxiae/mpd.nix
+++ atraphaxiae/mpd.nix
@@ -1,15 +1,18 @@
 { config, ... }:
 
 {
-	services.mpd = {
-		enable = true;
-		musicDirectory = "${config.home.homeDirectory}/Music";
-		extraConfig = ''
-			replaygain "track"
-			audio_output {
-				type "pipewire"
-				name "PipeWire Sound Server"
-			}
-		'';
+	services = {
+		mpd = {
+			enable = true;
+			musicDirectory = "${config.home.homeDirectory}/Music";
+			extraConfig = ''
+				replaygain "track"
+				audio_output {
+					type "pipewire"
+					name "PipeWire Sound Server"
+				}
+			'';
+		};
+		mpd-discord-rpc.enable =  true;
 	};
 }
