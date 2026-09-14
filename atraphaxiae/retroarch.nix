e953d92ab5105f040bf94e1a788dd21645513691
Nile Jocson <atraphaxiae@gmail.com>
2026-09-15 05:33:38 +0800

    Use retroarch-full, update flake.lock

--- atraphaxiae/retroarch.nix
+++ atraphaxiae/retroarch.nix
@@ -1,8 +1,9 @@
-{ config, pkgs, ... }:
+{ pkgs, ... }:
 
 {
 	programs.retroarch = {
 		enable = true;
+		package = pkgs.retroarch-full;
 		cores = {
 			ppsspp.enable = true;
 			mgba.enable = true;
